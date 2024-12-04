6a6f974f8d36329fe53f30a9d1a07223d74200da
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-12-04 20:28:53 +0530

    Created GlobalState & AppReducer

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -4,10 +4,12 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 import './App.css';
 
 function App() {
   return (
+    <GlobalProvider>
     <div>
       <Header />
       <div className="container">
@@ -16,7 +18,8 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+      </div>
+      </GlobalProvider>
   );
 }
 
