9c7cbf7b12d8e10b7710e012f0fcb8c9cced94a7
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2024-12-04 18:17:06 +0530

    final commit

---
+++ my_app/src/AddTransaction.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function AddTransaction() {
+    const [text, setText] = useState();
+    const [amount, setAmount] = useState();
+    return (
+        <>
+     <h3>Add new transaction</h3>
+      <htmlForm>
+        <div className="Form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        </div>
+        <div className="Form-control">
+          <label htmlFor="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+          >
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </htmlForm> 
+      </>
+      
+  )
+}
